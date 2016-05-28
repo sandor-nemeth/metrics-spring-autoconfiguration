f696227b0bcd6598c55a7c5b99aa840201a5baae
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2016-05-28 08:28:46 +0200

    Create initial project

---
+++ src/main/java/io/github/sandor_nemeth/metrics/App.java
@@ -0,0 +1,13 @@
+package io.github.sandor_nemeth.metrics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
